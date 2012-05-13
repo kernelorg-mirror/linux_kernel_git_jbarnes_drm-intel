7ba746aaf7fc366ae9df28f76e29b94ed46245e1
Chris Wilson <chris@chris-wilson.co.uk>
2012-05-13 14:06:29 +0100

    simply-flush-invalidate

--- drivers/gpu/drm/i915/i915_gem_execbuffer.c
+++ drivers/gpu/drm/i915/i915_gem_execbuffer.c
@@ -156,39 +156,31 @@ i915_gem_object_set_to_gpu_domain(struct drm_i915_gem_object *obj,
 				  struct intel_ring_buffer *ring,
 				  struct change_domains *cd)
 {
-	uint32_t invalidate_domains = 0, flush_domains = 0;
+	uint32_t invalidate_domains, flush_domains;
 
 	/*
 	 * If the object isn't moving to a new write domain,
 	 * let the object stay in multiple read domains
 	 */
-	if (obj->base.pending_write_domain == 0)
+	if (obj->base.pending_write_domain)
+		cd->flips |= atomic_read(&obj->pending_flip);
+	else
 		obj->base.pending_read_domains |= obj->base.read_domains;
 
 	/*
-	 * Flush the current write domain if
-	 * the new read domains don't match. Invalidate
-	 * any read domains which differ from the old
-	 * write domain
+	 * Flush any data from the CPU caches.
 	 */
-	if (obj->base.write_domain &&
-	    (((obj->base.write_domain != obj->base.pending_read_domains ||
-	       obj->ring != ring)) ||
-	     (obj->fenced_gpu_access && !obj->pending_fenced_gpu_access))) {
-		flush_domains |= obj->base.write_domain;
-		invalidate_domains |=
-			obj->base.pending_read_domains & ~obj->base.write_domain;
-	}
+	flush_domains = 0;
+       	if (obj->base.pending_read_domains != obj->base.write_domain)
+		flush_domains = obj->base.write_domain;
+	if (flush_domains & I915_GEM_DOMAIN_CPU)
+		i915_gem_clflush_object(obj);
+
 	/*
 	 * Invalidate any read caches which may have
 	 * stale data. That is, any new read domains.
 	 */
-	invalidate_domains |= obj->base.pending_read_domains & ~obj->base.read_domains;
-	if ((flush_domains | invalidate_domains) & I915_GEM_DOMAIN_CPU)
-		i915_gem_clflush_object(obj);
-
-	if (obj->base.pending_write_domain)
-		cd->flips |= atomic_read(&obj->pending_flip);
+	invalidate_domains = obj->base.pending_read_domains & ~obj->base.read_domains;
 
 	/* The actual obj->write_domain will be updated with
 	 * pending_write_domain after we emit the accumulated flush for all
